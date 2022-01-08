ee4058ef77b9eb9d3c5ebfa459f8ac4643ce6fc0
deepali <deepaalipatil@gmail.com>
2022-01-08 18:16:25 +0530

    ADDED:Junit parameterized test to validate multiple email

--- src/test/java/com/bridgelabz/regexjunittest/UserRegistration.java
+++ src/test/java/com/bridgelabz/regexjunittest/UserRegistration.java
@@ -3,36 +3,47 @@ package com.bridgelabz.regexjunittest;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-	private static final String FirstNameRegex= "^[A-Z][a-z]{2,}";
-	private static final String LastNameRegex= "^[A-Z][a-z]{2,}";
-	private static final String EmailRegex = "^[A-Za-z0-9_-]+([.][A-Za-z0-9_-]+)@[A-Za-z]+[A-Za-z]+([.][A-Za-z]+)";
-	private static final String PhoneNoRegex = "^[0-9]{2}[0-9]{10}";
-	private static final String PasswordRegex="^(?=.*[a-z])(?=."+ "*[A-Z])(?=.*[0-9])"+ "(?=.*[-+_!@#$%^&*., ?]).+$";
+	private static final String FIRST_NAME_REGEX = "^[A-Z][a-z]{2,}";
+	private static final String LAST_NAME_REGEX = "^[A-Z][a-z]{2,}$";
+	private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+-]+(?:\\.[a-zA-Z0-9_+-]+)*@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]{2,})*(?![a-zA-Z0-9.]+)*$";
+	private static final String PHONE_NUMBER_REGEX = "^[0-9]{2}[0-9]{10}";
+	private static final String PASSWORD_REGEX = "(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_-])[a-zA-Z0-9].{8,}$";
+	static boolean isFirstNameValid,isLastNameValid,isMailValid,isNumberValid,isPasswordValid,isMoodValid;
 	
-	public  boolean userFirstName(String firstName) {
-
-		return Pattern.matches(FirstNameRegex, firstName);	
-    
-   }
+	public boolean validateFirstName(String firstName) {
+		isFirstNameValid= Pattern.matches(FIRST_NAME_REGEX,firstName);
+		if(!isFirstNameValid) {
+			System.out.println("First Name is invalid...Please enter proper first name");
+		}
+		return isFirstNameValid;
+	}
+	public boolean validateLastName(String lastName) {
+		isLastNameValid= Pattern.matches(LAST_NAME_REGEX, lastName);
+		if(!isLastNameValid) {
+			System.out.println("Last Name is invalid...Please enter proper last name");
+		}
+		return isLastNameValid;
+	}
+	public boolean validateEmail(String mail) {
+		isMailValid= Pattern.matches(EMAIL_REGEX, mail);
+		if(!isMailValid) {
+			System.out.println("E-mail is invalid...Please enter e-mail");
+		}
+		return isMailValid;
+	}
+	public boolean validatePhoneNumber(String number) {
+		boolean isNumberValid= Pattern.matches(PHONE_NUMBER_REGEX, number);
+		if(!isNumberValid) {
+			System.out.println("Phone Number is invalid...Please enter proper phone-number");
+		}
+		return isNumberValid;
+	}
+	public boolean validatePassword(String password) {
+		isPasswordValid= Pattern.matches(PASSWORD_REGEX, password);
+		if(!isPasswordValid) {
+			System.out.println("Password is invalid...Please enter proper password");
+		}
+		return isPasswordValid;
+	}
 	
-	public  boolean userLastName(String lastName) {
-
-		return Pattern.matches(LastNameRegex, lastName);		
-
-	}
-
-	public boolean userEmail(String email) {
-		
-		return Pattern.matches(EmailRegex, email);
-	}
-		
-    public boolean userPhoneNo(String phone) {
-		
-		return Pattern.matches(PhoneNoRegex, phone);
-	}
-		
-    public boolean userPassword(String password) {
-		
-		return Pattern.matches(PasswordRegex, password);
-	}
 }
