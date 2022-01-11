d71b485bce8a209426f516a919498627c946f50a
deepaalip <95680526+deepaalip@users.noreply.github.com>
2022-01-11 09:45:32 +0530

    Delete ValidatePhoneNumber.java

--- src/test/java/com/bridgelabz/regexjunittest/ValidatePhoneNumber.java
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.regexjunittest;
-@FunctionalInterface
-public interface ValidatePhoneNumber {
-public boolean validatePhoneNumber(String number)throws  UserValidationException;
-}
