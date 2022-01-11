d58e24e2144d18c6e7cb9eae235f964888185658
deepaalip <95680526+deepaalip@users.noreply.github.com>
2022-01-11 09:44:44 +0530

    Delete ValidateFirstName.java

--- src/test/java/com/bridgelabz/regexjunittest/ValidateFirstName.java
+++
@@ -1,5 +0,0 @@
-package com.bridgelabz.regexjunittest;
-@FunctionalInterface
-public interface ValidateFirstName {
-public boolean validateFirstName(String firstName) throws UserValidationException;
-}
